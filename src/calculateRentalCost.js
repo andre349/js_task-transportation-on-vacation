d88cf5d4dbb958abf2a3a5d33c52704eaab65c0c
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-02-12 19:00:30 +0100

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+  let total = days * pricePerDay;
+
+  if (days >= 3 && days <= 6) {
+    total -= 20;
+  } else if (days >= 7) {
+    total -= 50;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
